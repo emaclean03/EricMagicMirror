e037e4f1b4c1f6b323d977f197025c2eababce4e
Eric <>
2023-08-25 14:39:21 -0400

    Adding log

--- modules/MMM-GoogleImages/node_helper.js
+++ modules/MMM-GoogleImages/node_helper.js
@@ -1,14 +1,20 @@
 const NodeHelper = require("node_helper");
 const request = require("request");
+const { Log } = require("../../module-types");
 
 module.exports = NodeHelper.create({
 	socketNotificationReceived: function (notification, payload) {
 		/*Sends the payload back to broadcast to clients*/
 		var self = this;
 
-		request("https://photos.app.goo.gl/6vPWySmZZ6bcesT26", function (error, response, body) {
+		/*request("https://photos.app.goo.gl/6vPWySmZZ6bcesT26", function (error, response, body) {
 			console.log("PHOTO: ", extractPhotos(body));
 			self.sendSocketNotification("Image", extractPhotos(body));
+		});*/
+
+		fetch("https://photos.app.goo.gl/6vPWySmZZ6bcesT26").then((response) => {
+			Log.log("Response: ", response);
+			self.sendNotification("Image", extractPhotos(response.body));
 		});
 	},
 
